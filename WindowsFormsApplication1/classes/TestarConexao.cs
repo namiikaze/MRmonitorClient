a2fcb0397720901cd5cbc22b5292ebf6b051b6db
Matheus <matheus.rangel10@hotmail.com>
2019-08-21 15:21:58 -0300

    proxy.

---
+++ WindowsFormsApplication1/classes/TestarConexao.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MRmonitorClient.classes
+{
+    class TestarConexao
+    {
+        public bool Aberta(string hostname, int porta)
+        {
+            TcpClient tcp = new TcpClient();
+            bool PortaAberta = true;
+            try
+            {
+                tcp.Connect(hostname, porta);
+                tcp.Close();
+            }
+            catch
+            {
+                PortaAberta = false;
+            }
+            return PortaAberta;
+        }
+
+    }
+}
