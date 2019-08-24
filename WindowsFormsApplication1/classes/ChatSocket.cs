6539edacbd5635adf31ed8ab69d9f54e9ced2da3
Matheus <matheus.rangel10@hotmail.com>
2019-08-24 12:41:27 -0300

    chat

---
+++ WindowsFormsApplication1/classes/ChatSocket.cs
@@ -0,0 +1,60 @@
+﻿using MRmonitorClient.view;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Collections.Generic;
+using System.Web.Script.Serialization;
+
+namespace MRmonitorClient.classes
+{
+    
+    class ChatSocket
+    {
+        ObjectThread objThread = new ObjectThread();
+        string MeuSocketID;
+        Quobject.SocketIoClientDotNet.Client.Socket socket;
+        public ChatSocket(Quobject.SocketIoClientDotNet.Client.Socket socket, Chat form)
+        {
+            this.socket = socket;
+
+           
+            socket.On("MensagemRecebida", (data) =>
+            {
+
+                
+            });
+            
+            socket.On("users", (data) =>
+            {
+                
+            });
+           
+
+
+            socket.On("ClientId", (data) =>
+            {
+                MeuSocketID = data.ToString();
+                form.msgRecebida(MeuSocketID);
+            });
+        }
+
+        public void Mensagem(string msg,string destino)
+        {
+            socket.Emit("EnviarMensagem", EnviarJsonMessage(msg,destino));   
+        }
+
+
+        public string EnviarJsonMessage(string msg, string destino)
+        {
+            string retorno = "{ \"mensagem\": \""+msg +"\", " +
+            "\"destino\": \""+destino+"\" }";
+            return retorno;
+        }
+
+
+    }
+}
